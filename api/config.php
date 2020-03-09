b67c83c1e2f1595c722494d1cebde1d129041cbc
erwinarguellocampos <61994874+erwinarguellocampos@users.noreply.github.com>
2020-03-09 16:40:05 -0600

    Initial commit

---
+++ api/config.php
@@ -0,0 +1,6 @@
+<?php
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_PORT', '3306');
+define('MYSQL_USER', 'soporteti');
+define('MYSQL_PASSWORD', '$@Grupo0rve@$');
+define('MYSQL_DATABASE', 'gorveti');
